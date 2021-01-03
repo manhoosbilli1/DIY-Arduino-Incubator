1578f98fba6c08594ffd2e20c34d168b2c7433a3
manhoosbilli1 <shoaib.mustafa7@hotmail.com>
2021-01-03 22:41:33 +0500

    Initial testing of modules and cirucit

---
+++ src/main.cpp
@@ -0,0 +1,207 @@
+#include <Arduino.h> //libraries
+#include <Wire.h>
+#include <LiquidCrystal_I2C.h>
+#include "DHT.h"
+#include <OneWire.h>
+#include <DallasTemperature.h>
+
+#define DHTTYPE DHT22
+#define DHTPIN 7
+#define ONE_WIRE_BUS 8
+
+//Object initiation
+DHT dht(DHTPIN, DHTTYPE);
+OneWire oneWire(ONE_WIRE_BUS);
+DallasTemperature sensors(&oneWire);
+DeviceAddress insideThermometer;
+LiquidCrystal_I2C lcd(0x27, 16, 2); // set the LCD address to 0x27 for a 16 chars and 2 line display
+
+//variables
+volatile boolean TurnDetected;
+volatile boolean up;
+#define clk 3
+#define dt 4
+#define sw 5
+int counter, arrow, prevCounter, counterMax = 5, counterMin = 0;
+const byte debounce_D = 10;
+bool pressed = 1, updateSensor = 0, updateMenu = 0;
+float f, t, h, ds_t;
+//struct  and enums
+
+enum menu
+{
+  home = 0,
+  page1 = 1,
+  page2 = 2,
+  page3 = 3,
+  page4 = 5
+} page;
+
+//function definition
+void isr0();
+void menu();
+void sensor_handler();
+void rotary_handler();
+void display_handler(
+    int sensor_update = 0,
+    int arrowPos = 0,
+    String menu1 = "menu",
+    String menu2 = "menu",
+    int menu1x = 1,
+    int menu1y = 0,
+    int menu2x = 1,
+    int menu2y = 1);
+
+void setup()
+{
+  lcd.init();
+  lcd.backlight();
+  Serial.begin(2000000);
+  //begin stuff
+  dht.begin();
+  sensors.begin();
+  //pin intiation
+  pinMode(LED_BUILTIN, OUTPUT);
+  pinMode(clk, INPUT);
+  pinMode(dt, INPUT);
+  pinMode(sw, INPUT);
+  attachInterrupt(digitalPinToInterrupt(clk), isr0, CHANGE);
+  delay(100);
+}
+
+void loop()
+{
+  rotary_handler();
+  sensor_handler(); //handles requesting data from sensors
+
+  switch (counter) //handles menu
+  {
+  case home:
+    //display sensors
+    display_handler(1); //this updates only sensor vaues on screen
+    break;
+
+  case page1:
+    display_handler(0, 0, "menu1", "menu2");
+    break;
+
+  case page2:
+    display_handler(0, 1, "menu1", "menu2");
+    break;
+
+  case page3:
+    display_handler(0, 0, "menu3", "menu4");
+    break;
+
+  case page4:
+    display_handler(0, 1, "menu3", "menu4");
+    break;
+  }
+}
+
+void isr0()
+{
+  TurnDetected = true;
+  up = (digitalRead(clk) == digitalRead(dt));
+}
+void rotary_handler()
+{
+  if (TurnDetected)
+  {
+    if (up)
+    {
+      if (counter != counterMax)
+      {
+        counter++;
+      }
+      else
+      {
+        counter = 0;
+      }
+    }
+    else
+    {
+      if (counter != counterMin)
+      {
+        counter--;
+      }
+      else
+      {
+        counter = counterMax;
+      }
+    }
+    TurnDetected = false;
+  }
+  if (prevCounter != counter)
+  {
+    prevCounter = counter;
+    updateMenu = true;
+  }
+}
+
+void display_handler(
+    int sensor_update,
+    int arrowPos,
+    String menu1, 
+    String menu2,
+    int menu1x,
+    int menu1y,
+    int menu2x,
+    int menu2y)
+{
+  if (counter > 0 && updateMenu == true)
+  {
+    lcd.clear();
+    if (arrowPos == 0)
+    {
+      lcd.setCursor(0, 0);
+      lcd.print(">");
+    }
+    if (arrowPos == 1)
+    {
+      lcd.setCursor(0, 1);
+      lcd.print(">");
+    }
+    lcd.setCursor(menu1x, menu1y);
+    lcd.print(menu1);
+    lcd.setCursor(menu2x, menu2y);
+    lcd.print(menu2);
+    updateMenu = false;
+  }
+
+  if (sensor_update == 1 && counter == 0)
+  {
+    if (updateSensor)
+    {
+      lcd.clear();
+      lcd.setCursor(0, 0);
+      lcd.print("T: ");
+      lcd.print(t);
+      lcd.setCursor(9, 0);
+      lcd.print("H: ");
+      lcd.print(h);
+      lcd.setCursor(0, 1);
+      lcd.print("DS_T: ");
+      lcd.print(ds_t);
+      updateSensor = false;
+    }
+  }
+}
+
+void sensor_handler()
+{
+  volatile unsigned long currentTime = millis();
+  volatile unsigned long prevTime;
+  if ((currentTime - prevTime) >= 2000)
+  {
+    //dht sensor values
+    h = dht.readHumidity();
+    t = dht.readTemperature();
+    f = dht.readTemperature(true);
+    //ds18b20 value
+    ds_t = sensors.getTempCByIndex(0);
+    prevTime = currentTime;
+    updateSensor = true;
+  }
+}
+

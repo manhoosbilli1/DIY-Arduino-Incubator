c79a0d8b4523d693b32110e485b9e73fe1f6c280
manhoosbilli1 <shoaib.mustafa7@hotmail.com>
2021-01-09 00:38:24 +0500

    Working temp pid controller

--- src/main.cpp
+++ src/main.cpp
@@ -4,7 +4,8 @@
 #include "DHT.h"
 #include <OneWire.h>
 #include <DallasTemperature.h>
-
+#include <TimerOne.h>
+#include <PID_v1.h>
 //variables
 #define clk 3
 #define dt 4
@@ -12,45 +13,54 @@
 #define ONE_WIRE_BUS 8
 #define DHTPIN 7
 #define DHTTYPE DHT22
+#define heater 6
+
+double Setpoint = 28.71, h_setPoint = 60.0, Input, Output;
+
+double Kp = 10, Ki = 5, Kd =1.8;
+
+unsigned long currentTime = 0, lastTime = 0;
+
+volatile boolean TurnDetected, up;
+
+int counter, arrow, prevCounter, counterMax = 5, counterMin = 0, incubationPeriod = 21, counter1 = 0;
+
+bool pressed = 1, updateSensor = 0, updateMenu = 0;
+
+float f, t, h, curTemp;
 
 //Object initiation
 DHT dht(DHTPIN, DHTTYPE);
+
 OneWire oneWire(ONE_WIRE_BUS);
+
 DallasTemperature sensors(&oneWire);
+
 DeviceAddress insideThermometer;
+
+PID myPID(&Input, &Output, &Setpoint, Kp, Ki, Kd, DIRECT);
+
 LiquidCrystal_I2C lcd(0x27, 16, 2); // set the LCD address to 0x27 for a 16 chars and 2 line display
 
-unsigned long currentTime = 0, lastTime = 0;
-volatile boolean TurnDetected, up;
-int counter, arrow, prevCounter, counterMax = 5, counterMin = 0, pidInterval = 2100;
-bool pressed = 1, updateSensor = 0, updateMenu = 0;
-float f, t, h, ds_t, t_setPoint = 37.5, h_setPoint = 60, incubationPeriod = 21, curTemp;
-//PID configuration
-float KP = 0.0;
-float KI = 0.0;
-float KD = 0.0;
-float last_error;
-float errSum;
-float output = 0;
-float GOAL = 0;
-unsigned long T;
-
 //structs  and enums
-
 enum menu
 {
   home = 0,
   page1 = 1,
   page2 = 2,
   page3 = 3,
-  page4 = 5
+  page4 = 4
 } page;
 
 //function definition
-void isr0();
+void isr1();
+
 void menu();
+
 void sensor_handler();
+
 void rotary_handler();
+
 void display_handler(
     int sensor_update = 0,
     int arrowPos = 0,
@@ -61,20 +71,36 @@ void display_handler(
     int menu2x = 1,
     int menu2y = 1);
 
+void compute();
+
+void count();
+
 void setup()
 {
-  lcd.init();
-  lcd.backlight();
-  Serial.begin(2000000);
   //begin stuff
   dht.begin();
+
+  lcd.init();
+
+  lcd.backlight();
+
   sensors.begin();
+
+  myPID.SetMode(AUTOMATIC);
+
+  myPID.SetSampleTime(2300);
+
   //pin intiation
   pinMode(LED_BUILTIN, OUTPUT);
+
   pinMode(clk, INPUT);
+
   pinMode(dt, INPUT);
+
   pinMode(sw, INPUT);
-  attachInterrupt(digitalPinToInterrupt(clk), isr0, CHANGE);
+
+  attachInterrupt(digitalPinToInterrupt(clk), isr1, CHANGE);
+
   delay(100);
 
   //run first time setup and detect if it is first time setup.
@@ -85,7 +111,8 @@ void loop()
   currentTime = millis();
   rotary_handler();
   sensor_handler(); //handles requesting data from sensors
-
+  myPID.Compute();
+  analogWrite(heater, Output);
   switch (counter) //handles menu
   {
   case home:
@@ -94,11 +121,11 @@ void loop()
     break;
 
   case page1:
-    display_handler(0, 0, "menu1", "menu2");
+    display_handler(0, 0, "set PID const", "menu2");
     break;
 
   case page2:
-    display_handler(0, 1, "menu1", "menu2");
+    display_handler(0, 1, "set PID const", "menu2");
     break;
 
   case page3:
@@ -108,10 +135,13 @@ void loop()
   case page4:
     display_handler(0, 1, "menu3", "menu4");
     break;
+
+  default:
+    counter = 0;
   }
 }
 
-void isr0()
+void isr1()
 {
   TurnDetected = true;
   up = (digitalRead(clk) == digitalRead(dt));
@@ -192,13 +222,20 @@ void display_handler(
       lcd.print("H: ");
       lcd.print(h);
       lcd.setCursor(0, 1);
-      lcd.print("DS_T: ");
-      lcd.print(ds_t);
+      lcd.print("Input:");
+      lcd.print(Input);
+      lcd.setCursor(13, 1);
+      lcd.print(Output); //printing output
       updateSensor = false;
     }
   }
 }
-
+void count()
+{
+  counter1++;
+  if (counter1 > 80)
+    counter1 = 0;
+}
 void sensor_handler()
 {
   volatile unsigned long currentTime = millis();
@@ -211,36 +248,9 @@ void sensor_handler()
     f = dht.readTemperature(true);
     //ds18b20 value
     sensors.requestTemperatures();
-    ds_t = sensors.getTempCByIndex(0);
-    curTemp = (t + ds_t) / 2; //returns average value between two.  TODO: make an if statement that this line will execute
-    prevTime = currentTime;   //only the user has chosen this mode of sensors. if user is using only one sensor.
-    updateSensor = true;      //equal currtemp to that only.
+    float ds_t = sensors.getTempCByIndex(0);
+    Input = (t + ds_t) / 2; //returns average value between two.  TODO: make an if statement that this line will execute
+    prevTime = currentTime;    //only the user has chosen this mode of sensors. if user is using only one sensor.
+    updateSensor = true;       //equal currtemp to that only.
   }
-}
-void temp_controller()                             //only use this format if using timer interrupt. otherwise time it
-{
-  unsigned long time = millis();                   //record the current time
-
-  float timeChange = (float)(time - lastTime);     //calculate time change since last executed
-
-  float errorT = t_setPoint - curTemp;             //calculate error for average temp
-
-  errSum += (errorT * timeChange);                 //will increase with time
-
-  float dErr = (errorT - last_error) / timeChange; //time is ommitted from these equation cause timechange is constant
-
-  output = KP * errorT + KI * errSum + KD * dErr;  //output will be delay feeding into mosfet or diac. 0-255 output pwm value
-
-  last_error = errorT;                             //save last error for differential
-
-  lastTime = time;                                 //save time for finding delta time
-}
-//need to figure out the loop time. should be around 2s because thats when sensors values are going to be updated
-
-void setPIDK(float kp, float ki, float kd)
-{
-  KP = kp;
-  KI = ki;
-  KD = kd;
-  //TODO: write this to EEPROM to remember.
 }
